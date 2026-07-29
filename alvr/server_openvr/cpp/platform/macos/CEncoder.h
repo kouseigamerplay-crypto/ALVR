2eac6591ad4f467019dff5cd9a2aac379461b2da
HANAE ORITA <hanaeorita@macbookair.local>
2026-07-29 10:12:58 +0900

    Add macOS encoder API stubs

--- alvr/server_openvr/cpp/platform/macos/CEncoder.h
+++ alvr/server_openvr/cpp/platform/macos/CEncoder.h
@@ -4,12 +4,17 @@
 
 class CEncoder : public CThread {
 public:
-    CEncoder() { }
-    ~CEncoder() { }
+    CEncoder() = default;
+    ~CEncoder() = default;
+
     bool Init() override { return true; }
     void Run() override { }
 
     void Stop() { }
     void OnStreamStart() { }
     void InsertIDR() { }
+
+    void NewFrameReady() { }
+    void WaitForEncode() { }
+    void CaptureFrame() { }
 };
